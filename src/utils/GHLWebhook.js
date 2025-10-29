62437a58cc6a341aab9f66ade00b8b0bd480d2e0
eatsalad239 <dan@doorknockingsucks.com>
2025-10-29 18:22:55 -0500

    Update GHLWebhook.js to implement ES6 module format and methods

--- src/utils/GHLWebhook.js
+++ src/utils/GHLWebhook.js
@@ -1,28 +1,41 @@
-'use strict';
+export default class GHLWebhook {
+    constructor() {
+        this.webhookUrl = 'https://services.leadconnectorhq.com/hooks/AEybRWw8cgDIMKRzNa0T/webhook-trigger/54ff1e67-5ee9-41f5-a0ba-d35943ec44d2';
+    }
 
-function sendLead(trackingId, leadData) {
-    const flatData = {
-        firstName: leadData.firstName,
-        lastName: leadData.lastName,
-        email: leadData.email,
-        phone: leadData.phone,
-        address: leadData.address,
-        source: leadData.source,
-        formType: leadData.formType,
-        timestamp: leadData.timestamp
-    };
-    const url = getWebhookUrl(trackingId);
-    return fetch(url, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(flatData)
-    });
+    async sendLead(leadData) {
+        return await this.sendRequest('sendLead', leadData);
+    }
+
+    async sendPartnerApplication(applicationData) {
+        return await this.sendRequest('sendPartnerApplication', applicationData);
+    }
+
+    async sendAppointment(appointmentData) {
+        return await this.sendRequest('sendAppointment', appointmentData);
+    }
+
+    async trackFormAbandonment(abandonmentData) {
+        return await this.sendRequest('trackFormAbandonment', abandonmentData);
+    }
+
+    async trackEngagement(engagementData) {
+        return await this.sendRequest('trackEngagement', engagementData);
+    }
+
+    async sendRequest(action, data) {
+        try {
+            const response = await fetch(this.webhookUrl, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ action, data }),
+            });
+            return await response.json();
+        } catch (error) {
+            console.error('Error sending request:', error);
+            throw error;
+        }
+    }
 }
-
-function getWebhookUrl(trackingId) {
-    return 'https://services.leadconnectorhq.com/hooks/AEybRWw8cgDIMKRzNa0T/webhook-trigger/54ff1e67-5ee9-41f5-a0ba-d35943ec44d2';
-}
-
-module.exports = { sendLead, getWebhookUrl };
